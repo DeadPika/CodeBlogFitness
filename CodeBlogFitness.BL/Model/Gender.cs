92e20091a3e03b0c9ad31a1ef0b9a05a71e8e501
FXCK DeadPika <xleb338@gmail.com>
2023-08-22 19:28:57 +0500

    Создана модель Пользователя.

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя полаю </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name) 
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пумтым или null", nameof(name));
+            }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
