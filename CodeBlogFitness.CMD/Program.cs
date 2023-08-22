4c4a8f4c77f2feaf211a669283d3a1872d3d0901
FXCK DeadPika <xleb338@gmail.com>
2023-08-22 20:47:17 +0500

    Конец первой практики.

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeBlogFitness.BL.Controller;
+using System;
 
 namespace CodeBlogFitness.CMD
 {
@@ -6,6 +7,25 @@ namespace CodeBlogFitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вас приветствует приложение CodeBlogeFitness от Gavr`а.");
+
+            Console.WriteLine("Введите имя Пользователя: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол: ");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения: ");
+            var birthDate = DateTime.Parse(Console.ReadLine()); // TODO: переписать
+
+            Console.WriteLine("Введите вес: ");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост: ");
+            var height = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
         }
     }
 }
